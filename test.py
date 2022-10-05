3597a5e019c1ddbd78d5dd72932cc05a02f64dca
khimnguynn <khimnguynn@gmail.com>
2022-10-05 10:56:57 +0700

    edit detach mode

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 import subprocess
 
-subprocess.Popen(["python3", "app.py"], shell=True, stdin=subprocess.PIPE, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, close_fds=True)
+subprocess.Popen(["nohup","python3", "app.py", "&"], shell=True, stdin=subprocess.PIPE, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, close_fds=True)
