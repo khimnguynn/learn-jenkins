361651d133545879f24468af2a6dd52040f6f75d
khimnguynn <khimnguynn@gmail.com>
2022-10-05 11:10:38 +0700

    test

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def home():
    return render_template('index.html')
+@app.route('/')
+def index():
+   return "HOME PAGE"
 
 
 if __name__ == '__main__':
