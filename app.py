ec4f2c40581b06f06635f19d940e6f90758d67cc
khimnguynn <khimnguynn@gmail.com>
2022-10-05 10:15:21 +0700

    fix app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def home():
    return render_template('index.html')
 @app.route('/test')
 def test():
-   return "Hello world v5a"
+   return "Hello world v5"
 
 if __name__ == '__main__':
    app.run("0.0.0.0", port=8080)
