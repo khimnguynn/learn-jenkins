c2ba1c9a136ccf02e38226e9df2772cda0af4fc5
khimnguynn <khimnguynn@gmail.com>
2022-10-05 10:50:31 +0700

    fixed file test

--- test.py
+++ test.py
@@ -1,2 +1,2 @@
-import os
-os.system("nohup python3 app.py &") 
+import subprocess
+subprocess.Popen(["python3", "app.py"], shell=True, stdin=subprocess.PIPE, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, close_fds=True)
